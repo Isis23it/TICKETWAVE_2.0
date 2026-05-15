3751b18ece005e73906e3e2f74fdbd6d224c5b7d
Isis23it <isachico2364@gmail.com>
2026-05-14 19:23:14 -0600

    feat: roles permisos middleware y policies [TW-23]

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration(false)
             ->colors([
                 'primary' => Color::Amber,
             ])
